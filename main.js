4c9690ce69d21b938491bfbfdada393391c63a58
Student_11 <jhonymaurad@gmail.com>
2018-10-23 11:52:44 -0400

    succesfully chnaged from an array of objects to a multidimensional array

--- main.js
+++ main.js
@@ -1,66 +1,80 @@
 //get the board
 const boardElement = document.querySelector('.board');
 const cells = [
-  {column: 0,row:0, value :0},
-  {column: 1,row:0, value :0},
-  {column: 2,row:0, value :0},
-  {column: 3,row:0, value :0},
-  {column: 4,row:0, value :0},
-  {column: 5,row:0, value :0},
-  {column: 6,row:0, value :0},
+  [0,0],
+  [1,0],
+  [2,0],
+  [3,0],
+  [4,0],
+  [5,0],
+  [6,0],
 
-  {column: 0,row:1, value :0},
-  {column: 1,row:1, value :0},
-  {column: 2,row:1, value :0},
-  {column: 3,row:1, value :0},
-  {column: 4,row:1, value :0},
-  {column: 5,row:1, value :0},
-  {column: 6,row:1, value :0},
+  [0,1],
+  [1,1],
+  [2,1],
+  [3,1],
+  [4,1],
+  [5,1],
+  [6,1],
 
-  {column: 0,row:2, value :0},
-  {column: 1,row:2, value :0},
-  {column: 2,row:2, value :0},
-  {column: 3,row:2, value :0},
-  {column: 4,row:2, value :0},
-  {column: 5,row:2, value :0},
-  {column: 6,row:2, value :0},
+  [0,2],
+  [1,2],
+  [2,2],
+  [3,2],
+  [4,2],
+  [5,2],
+  [6,2],
 
-  {column: 0,row:3, value :0},
-  {column: 1,row:3, value :0},
-  {column: 2,row:3, value :0},
-  {column: 3,row:3, value :0},
-  {column: 4,row:3, value :0},
-  {column: 5,row:3, value :0},
-  {column: 6,row:3, value :0},
+  [0,3],
+  [1,3],
+  [2,3],
+  [3,3],
+  [4,3],
+  [5,3],
+  [6,3],
 
-  {column: 0,row:4, value :0},
-  {column: 1,row:4, value :0},
-  {column: 2,row:4, value :0},
-  {column: 3,row:4, value :0},
-  {column: 4,row:4, value :0},
-  {column: 5,row:4, value :0},
-  {column: 6,row:4, value :0},
+  [0,4],
+  [1,4],
+  [2,4],
+  [3,4],
+  [4,4],
+  [5,4],
+  [6,4],
 
-  {column: 0,row:5, value :0},
-  {column: 1,row:5, value :0},
-  {column: 2,row:5, value :0},
-  {column: 3,row:5, value :0},
-  {column: 4,row:5, value :0},
-  {column: 5,row:5, value :0},
-  {column: 6,row:5, value :0}
+  [0,5],
+  [1,5],
+  [2,5],
+  [3,5],
+  [4,5],
+  [5,5],
+  [6,5],
 ];
+//set current player to 1 at the star of game
+let currentPlayer = 1;
+//update players turn
+function updatePlayer (){
+  if (currentPlayer === 1) {
+    currentPlayer = 2;
+  }
+  else{
+    currentPlayer = 1;
+  }
+}
 
 //render board with 42 cells
 const  renderBoard = () =>{
   for (let i = 0; i < cells.length; i++) {
     const cell = cells[i];
     const cellElement = document.createElement('div');
-    cellElement.style.left = (cell.column * 100).toString()+'px';
-    cellElement.style.top = (cell.row * 100).toString()+'px';
+    cellElement.style.left = (cell[0] * 100).toString()+'px';
+    cellElement.style.top = (cell[1] * 100).toString()+'px';
     cellElement.className = 'cell';
     //set a new attribute to each div
     cellElement.setAttribute('data-location', i);
     boardElement.appendChild(cellElement);
+    let x = cellElement.getAttribute('data-location', i);
+    cellElement.innerHTML = 'col: '+cell[0] +', row: '+ cell[1] + ' '+ x;
+
   }
 }
 renderBoard();// create board 7 colums by 6 rows
